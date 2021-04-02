048fa2034e0c4d1172555ea8764601a03d812b20
Wikus <21053197@sun.ac.za>
2021-04-02 18:47:12 +0200

    Minor changes to ConwaysByWikus

--- src/main/ConwaysByWikus.java
+++ src/main/ConwaysByWikus.java
@@ -4,21 +4,35 @@ import java.awt.*;
 
 public class ConwaysByWikus implements ConwaysGameOfLife {
 
-    public ConwaysByWikus() {
+    final int NUM_ROWS;
+    final int NUM_COLUMNS;
+
+    boolean[][] currentGrid;
+    boolean[][] nextGrid;
+
+    public ConwaysByWikus(int rows, int columns) {
+        this.NUM_ROWS = rows;
+        this.NUM_COLUMNS = columns;
+        this.currentGrid = new boolean[rows][columns];
+        this.nextGrid = new boolean[rows][columns];
     }
 
+    // Returns true if the given cell is alive and has fewer than two live neighbours
     public boolean liveCellWithFewerThanTwoLiveNeighboursDies(Point point) {
         return true;
     }
 
+    // Returns true if the given cell is alive and has two or three live neighbours
     public boolean liveCellWithTwoOrThreeLiveNeighboursLives(Point point) {
         return true;
     }
 
+    // Returns true if the given cell is alive and has more than three live neighbours
     public boolean liveCellWithMoreThanThreeLiveNeighboursDies(Point point) {
         return true;
     }
 
+    // Returns true if the given cell is dead and has exactly three live neighbours
     public boolean deadCellWithExactlyThreeLiveNeighboursBecomesAlive(Point point) {
         return true;
     }
